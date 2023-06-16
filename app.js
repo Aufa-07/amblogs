5a217c514eec71f839343e8f0cbe4f4954d2d246
Aufa-07 <85112948+Aufa-07@users.noreply.github.com>
2023-06-16 20:23:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
